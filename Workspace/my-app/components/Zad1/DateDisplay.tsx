dbef3f6df4fd2c0bcb6fdb89954d1a6d636db2b4
Adrian Kopczynski <adrian.kopczynski@onet.eu>
2026-01-04 02:12:05 +0100

    Zad1

---
+++ Workspace/my-app/components/Zad1/DateDisplay.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Text} from 'react-native'
+const DateDisplay = () => {
+    const date = new Date().toLocaleDateString();
+ return <Text>{date}</Text>;
+}
+
+export default DateDisplay;
